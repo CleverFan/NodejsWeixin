8b6b1acff8329337fde6f0a241fe3f95b5fc697b
17706446847 <377109151@qq.com>
2016-08-31 11:46:50 +0800

    add  message

--- routes/index.js
+++ routes/index.js
@@ -37,4 +37,69 @@ module.exports = function(app){
       return false;
     }
   });
+
+  app.post('/interface',function (req,res,next) {
+    var post_data="";
+
+    req.on("data",function(data){post_data=data;});
+    req.on("end",function(){
+      var xmlStr=post_data.toString('utf-8',0,post_data.length);
+      //解析消息代码
+      var xml=require('node-xml');
+      // 定义解析存储变量
+      var ToUserName="";
+      var FromUserName="";
+      var CreateTime="";
+      var MsgType="";
+      var Content="";
+      var tempName="";
+      //开始解析消息
+      var parse=new xml.SaxParser(function(cb){
+        cb.onStartElementNS(function(elem,attra,prefix,uri,namespaces){
+          tempName=elem;
+        });
+        cb.onCharacters(function(chars){
+          chars=chars.replace(/(^\s*)|(\s*$)/g, "");
+          if(tempName=="CreateTime"){
+            CreateTime=chars;
+          }
+        });
+        cb.onCdata(function(cdata){
+          if(tempName=="ToUserName"){
+            ToUserName=cdata;
+          }else if(tempName=="FromUserName"){
+            FromUserName=cdata;
+          }else if(tempName=="MsgType"){
+            MsgType=cdata;
+          }else if(tempName=="Content"){
+            Content=cdata;
+          }
+          console.log(tempName+":"+cdata);
+        });
+        cb.onEndElementNS(function(elem,prefix,uri){
+          tempName="";
+        });
+        cb.onEndDocument(function(){
+          //按收到的消息格式回复消息
+          CreateTime=parseInt(new Date().getTime() / 1000);
+          var msg="";
+          if(MsgType=="text"){
+            msg="谢谢关注,你说的是:"+Content;
+            //组织返回的数据包
+            var sendMessage=' ' +
+                '<xml>' +
+                '<ToUserName><![CDATA['+FromUserName+']]></ToUserName>' +
+                '<FromUserName><![CDATA['+ToUserName+']]></FromUserName>' +
+                '<CreateTime>'+CreateTime+'</CreateTime>' +
+                '<MsgType><![CDATA[text]]></MsgType>' +
+                '<Content><![CDATA['+msg+']]></Content>' +
+                '</xml>';
+            res.send(sendMessage);
+          }
+        });
+      });
+      parse.parseString(xmlStr);
+    });
+  });
+
 }
